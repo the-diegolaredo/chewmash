5d5e1ba33a396618fcd22909b26d540fccc2664c
the-diegolaredo <151796216+the-diegolaredo@users.noreply.github.com>
2026-09-05 11:58:16 -0700

    Show centered welcome after setup

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../../../entrypoints/dashboard/style.css';
 import './web.css';
+import './polish.css';
 import { App } from './App';
 import { FirstRunGate } from './FirstRunGate';
+import { SessionWelcome } from './SessionWelcome';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FirstRunGate>
-      <App />
+      <SessionWelcome>
+        <App />
+      </SessionWelcome>
     </FirstRunGate>
   </StrictMode>,
 );
