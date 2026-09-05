e2566cf07b4fbbb3aa0ed3121ee65d536752306d
the-diegolaredo <151796216+the-diegolaredo@users.noreply.github.com>
2026-09-05 13:03:59 -0700

    Apply Nimbus Sans L to extension dashboard

--- entrypoints/dashboard/main.tsx
+++ entrypoints/dashboard/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './style.css';
+import '../../src/ui/nimbus.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
