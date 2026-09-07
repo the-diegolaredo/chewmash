8b69d2a4c39b1cb94cd6875e551bd80aa407b8dd
the-diegolaredo <151796216+the-diegolaredo@users.noreply.github.com>
2026-09-06 17:41:23 -0700

    Add Capacitor mobile configuration

---
+++ apps/mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.chewmash',
+  appName: 'ChewMash',
+  webDir: '../web/dist',
+};
+
+export default config;
